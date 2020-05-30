997c97af412de991d955d32dfcf18710e224f154
estefa942 <estefany.muriel@udea.edu.co>
2020-05-29 21:14:49 -0500

    added coments view

--- view.py
+++ view.py
@@ -19,49 +19,44 @@ path_bd = 'bd_procesada/*.png'
 
 
 def comparation(image):
+    #Se saca el histograma de la imagen que escogió el usuario
     histImage = cv2.calcHist([image], [0], None, [256], [150, 256])
-    eyes = []
+    eyes = [] #Definimos un arreglo para guardar los histogramas
     for eye in eyesList:
+        #Sacamos el histograma de cada ojo con el que vamos a comparar
         histImageDb = cv2.calcHist([eye[1]], [0], None, [256], [150, 256])
-        result = np.sqrt(np.power((np.subtract(histImage, histImageDb)), 2))
+        #Calculamos la distancia euclidiana entre el histograma de la imagen a comparar y la de la base de datos
         dst = distance.euclidean(histImage, histImageDb)
+        #Guardamos el path del ojo y su respectivo porcentaje de similitud
         eyes.append((eye[0], util.similarity_percentage(dst)))
+    #Ordenamos de menor a mayor
     eyes = sorted(eyes, key=lambda x: x[1])
+    #Tomamos el mejor porcentaje
     eye_select = eyes[len(eyes)-1]
     return (eye_select)
 
-
+#En este método es para mostrar la interfaz visual y relacionarla con la lógica previamente definida para la idenntificación de irirs
 def select_image():
-    global imageA, imageB, imageC
-    path = filedialog.askopenfilename()
-    print('entrada', path)
-    if len(path) > 0:
-        frame = cv2.imread(path)
-        iris = copy.copy(frame)
-        pupil = processing.getPupil(frame)
-        iris = processing.getIris(pupil)
-        polar = processing.getPolar2CartImage(iris)
-        for file in glob.glob(path_bd):
-            img = cv2.imread(file)
-            path = os.path.splitext(file)[0]
-            eyesList.append((path, img))
-        # cv2.imshow('polar', polar)
-
-        # frame2 = cv2.imread('bd/022R_1.png')
-        # iris2 = copy.copy(frame2)
-        # pupil2 = getPupil(frame2)
-        # # cv2.imshow('pupil', pupil2)
-        # iris2 = getIris(pupil2)
-        # # cv2.imshow('iris', iris2)
-        # polar2 = getPolar2CartImage(iris2)
-        # cv2.imwrite('bd_procesada/022R_1_polar.png', polar2)
-        eye_select = comparation(polar)
-        eye_percentage_select = eye_select[1]
-        final_eye = cv2.imread(util.get_eye_path(eye_select[0]))
-        # cv2.imshow('final', final_eye)
+    global imageA, imageB, imageC #Definimos unas variables globales que van a alojar las imágenes que mostraremos en la vista
+    path = filedialog.askopenfilename() #le asignamos a la variable path la ruta de la imagés seleccionada por el usuario
+    if len(path) > 0: #Si seleccionaron una imagen entramos a ejecutar los métodos de reconocimiento
+        frame = cv2.imread(path) #Asignamos a frame la variable seleccionada
+        iris = copy.copy(frame) #Hacemos una copia de la imagen anterior
+        pupil = processing.getPupil(frame) #Obtenemos la pupila del ojo
+        iris = processing.getIris(pupil) #Obtenemos el iris del ojo sin la pupila
+        polar = processing.getPolar2CartImage(iris) # Pasamos el círculo obtenido a coordenadas cartesianas
+        #Se hace un ciclo para obtener cada imagen de la base de datos ya previamente procesadas y compararlas con la nueva entrada
+        for file in glob.glob(path_bd): 
+            img = cv2.imread(file) #se asigna a img la imagen extraida de la base de datos
+            path = os.path.splitext(file)[0] #Se extrae el nombre del archivo
+            eyesList.append((path, img)) #Ee le pasa a este arreglo la imagen y la ruta del archivo para tener un registro
+        eye_select = comparation(polar) #Aquí se pasa la imagen que ingresó el usuario en coordenadas cartesianas para compararla con los otros ojos
+        eye_percentage_select = eye_select[1] #Obtenemos el mejor porcentaje  
+        final_eye = cv2.imread(util.get_eye_path(eye_select[0])) #Buscamos la foto del ojo con la mejor coincidencia 
        #To Pil format
-        dimensions = (500,400)
-        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        dimensions = (500,400) #Definimos las nuevas dimensiones de las imágenes 
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB) #Volvemos la imagen a su color original
+        #Le cambiamos las dimensiones a las imágenes ya que son muy grandes para mostrarlas en la aplicación
         frame = cv2.resize(frame, dsize=dimensions, interpolation=cv2.INTER_CUBIC)
         frame = Image.fromarray(frame)
         polar = cv2.resize(polar, dsize=(400,400), interpolation=cv2.INTER_CUBIC)
@@ -77,6 +72,7 @@ def select_image():
         final = ImageTk.PhotoImage(final) 
 
         if imageA is None or imageB is None or imageC is None:
+            #Configuramos los camppos para mostrar las imágenes y el texto
             imageA = tk.Label(image=frame)
             imageA.image = frame
             imageA.pack(side="left", padx=1, pady=1)
@@ -104,9 +100,6 @@ imageB = None
 imageC = None
 output = tk.StringVar()
 
-# create a button, then when pressed, will trigger a file chooser
-# dialog and allow the user to select an input image; then add the
-# button the GUI
 
 lbl = tk.Label(root, text='El porcentaje de similitud es de: ')
 lbl.pack()
@@ -114,7 +107,6 @@ out = tk.Entry(root, justify="center", textvariable=output,
                state="disabled").pack()
 btn = tk.Button(root, text="Select an image", command=select_image)
 btn.pack(side="bottom", fill="both", padx="5", pady="5")
-# btn.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
 # kick off the GUI
 root.mainloop()
 cv2.waitKey(0)
